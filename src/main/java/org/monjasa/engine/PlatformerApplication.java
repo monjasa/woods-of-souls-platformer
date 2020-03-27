2e5ea3145ee16c8e3da60d5ba8c8c7252a99d2f8
Monjasa <monja.nya@gmail.com>
2020-03-27 18:22:49 +0200

    v0.0.2 - Add Player and Platform Entities

--- src/main/java/org/monjasa/engine/PlatformerApplication.java
+++ src/main/java/org/monjasa/engine/PlatformerApplication.java
@@ -2,6 +2,19 @@ package org.monjasa.engine;
 
 import com.almasb.fxgl.app.GameApplication;
 import com.almasb.fxgl.app.GameSettings;
+import com.almasb.fxgl.entity.Entity;
+import com.almasb.fxgl.physics.BoundingShape;
+import com.almasb.fxgl.physics.HitBox;
+import com.almasb.fxgl.physics.PhysicsComponent;
+import com.almasb.fxgl.physics.box2d.dynamics.BodyType;
+import javafx.geometry.Point2D;
+import javafx.scene.Node;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+import java.awt.*;
+
+import static com.almasb.fxgl.dsl.FXGL.*;
 
 public class PlatformerApplication extends GameApplication {
 
@@ -10,8 +23,69 @@ public class PlatformerApplication extends GameApplication {
         settings.setWidth(1280);
         settings.setHeight(720);
         settings.setTitle("Woods of Souls");
-        settings.setVersion("0.0.1");
+        settings.setVersion("0.0.2");
     }
+
+    private Entity player;
+
+    @Override
+    protected void initGame() {
+
+        player = createPlayer(new Point2D(100, 100));
+
+        getGameWorld().addEntity(player);
+        getGameWorld().addEntity(createPlatform(new Point2D(80, 400), 200, 40));
+        getGameWorld().addEntity(createPlatform(new Point2D(360, 280), 280, 40));
+    }
+
+    private Entity createPlayer(Point2D position) {
+
+        Entity player = new Entity();
+        player.setPosition(position);
+
+        Node playerView = new Rectangle(50, 50, Color.DARKBLUE);
+        player.getViewComponent().addChild(playerView);
+
+        double viewWidth = playerView.getLayoutBounds().getWidth();
+        double viewHeight = playerView.getLayoutBounds().getHeight();
+
+        HitBox playerHitBox = new HitBox("VIEW", BoundingShape.box(viewWidth, viewHeight));
+        player.getBoundingBoxComponent().clearHitBoxes();
+        player.getBoundingBoxComponent().addHitBox(playerHitBox);
+
+        Point2D entityCenter = player.getBoundingBoxComponent().getCenterLocal();
+        player.getTransformComponent().setScaleOrigin(entityCenter);
+        player.getTransformComponent().setRotationOrigin(entityCenter);
+
+        PhysicsComponent playerPhysicsComponent = new PhysicsComponent();
+        playerPhysicsComponent.setBodyType(BodyType.DYNAMIC);
+        player.addComponent(playerPhysicsComponent);
+
+        return player;
+    }
+
+    private Entity createPlatform(Point2D position, double width, double height) {
+
+        Entity platform = new Entity();
+
+        platform.setPosition(position);
+
+        Node platformView = new Rectangle(width, height, Color.BLACK);
+        platform.getViewComponent().addChild(platformView);
+
+        HitBox platformHitBox = new HitBox("VIEW", BoundingShape.box(width, height));
+        platform.getBoundingBoxComponent().clearHitBoxes();
+        platform.getBoundingBoxComponent().addHitBox(platformHitBox);
+
+        Point2D entityCenter = platform.getBoundingBoxComponent().getCenterLocal();
+        platform.getTransformComponent().setScaleOrigin(entityCenter);
+        platform.getTransformComponent().setRotationOrigin(entityCenter);
+
+        platform.addComponent(new PhysicsComponent());
+
+        return platform;
+    }
+
 
     public static void main(String[] args) {
         launch(args);
